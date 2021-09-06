0e248db4239e6c6a6eb8be8c1737abfa49690e04
unknown <srujanmahaja5@gmail.com>
2021-09-06 13:15:28 +0530

    [Srujan] Add Welcome Message

---
+++ Linecomparision.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    class LineComparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.ReadLine();
+        }
+    }
+}
+
