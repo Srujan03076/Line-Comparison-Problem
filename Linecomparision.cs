96de94fcb65fc2d918cf0420ef74990ff015491c
unknown <srujanmahaja5@gmail.com>
2021-09-06 15:40:18 +0530

    [Srujan] Adding Equality of Two Lines

--- Linecomparision.cs
+++ Linecomparision.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace Line_Comparison
 {
@@ -9,16 +10,40 @@ namespace Line_Comparison
             //Welcome message
             Console.WriteLine("Welcome to Line Comparison Computation");
 
-            //UC_1_singleLineLength
+
+            //UC_2_equalityOfTwoLines
+
             double x1, x2, y1, y2;
+            double a1, a2, b1, b2;
+            Console.WriteLine("Enter co-ordinates of line1");
             Console.WriteLine("Enter co-ordinates (x1, y1): ");
             x1 = double.Parse(Console.ReadLine());
             y1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter co-ordinates (x2, y2): ");
             x2 = double.Parse(Console.ReadLine());
             y2 = double.Parse(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("The Length of the line is: " + length);
+            Console.WriteLine("Enter co-ordinates of line2");
+            Console.WriteLine("Enter co-ordinates (a1, b1): ");
+            a1 = double.Parse(Console.ReadLine());
+            b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinates (a2, b2): ");
+            a2 = double.Parse(Console.ReadLine());
+            b2 = double.Parse(Console.ReadLine());
+            double length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            double length2 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+
+            if (length1 == length2)
+            {
+                Console.WriteLine("The Length of two lines is equal.");
+            }
+            else
+            {
+                Console.WriteLine("The Length of two lines is not equal.");
+                Console.ReadLine();
+            }
+
+            
+
         }
     }
 }
